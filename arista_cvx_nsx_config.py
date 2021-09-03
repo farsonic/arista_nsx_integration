1a521ae0fa29deb5650a7f36ec8a73711c0c5381
Francois Prowse <fp@prowsehouse.com>
2021-09-03 18:37:43 +1000

    changed post to get

--- arista_cvx_nsx_config.py
+++ arista_cvx_nsx_config.py
@@ -121,7 +121,7 @@ def get_notification_id_from_nsx():
     print("Extracting notification ID from NSX-T Manager")
     url = "https://"+nsx_ip+"/api/v1/notification-watchers"
     payload = {
-        "method": "POST",
+        "method": "GET",
         "uri": "/pcs/v1/nsgroup/notification",
         "use_https": True,
         "certificate_sha256_thumbprint": cvx_thumbprint,
